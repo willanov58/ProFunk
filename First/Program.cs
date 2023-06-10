7c4c32d863e622b38ae90032262621493ca27bdb
Willanov <willanov@yandex.ru>
2023-06-10 13:55:50 +0500

    First

---
+++ First/Program.cs
@@ -0,0 +1,9 @@
+﻿void Pow(int a, int b)
+{
+    Console.WriteLine(Math.Pow(a,b));
+}
+System.Console.Write("Введите число A => ");
+int A =Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B => ");
+int B =Convert.ToInt32(Console.ReadLine());
+Pow(A,B);
