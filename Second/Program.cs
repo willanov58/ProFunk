d8d3de4949a2d5c46eb25e5b3a62d789baa3d8b0
Willanov <willanov@yandex.ru>
2023-06-10 14:14:33 +0500

    Second

---
+++ Second/Program.cs
@@ -0,0 +1,22 @@
+﻿void Sum(int n)
+{
+    int sum=0;
+   int count=0;
+   int temp=n;
+    while (temp!=0)
+    {
+        temp=temp/10;
+        count+=1;
+    }
+    for (int i=0;i<count;i++)
+    {
+        sum+=n%10;
+        n=n/10;
+    }
+    System.Console.WriteLine(sum);
+
+}
+Console.Clear();
+System.Console.Write("Введите число N => ");
+int N =Convert.ToInt32(Console.ReadLine());
+Sum(N);
