4e3065ece210dad2a3c0f7f3e255bed5b8d64db6
Willanov <willanov@yandex.ru>
2023-06-11 09:22:14 +0500

    Third

---
+++ Third/Program.cs
@@ -0,0 +1,27 @@
+﻿void Massive(string[] massive)
+{
+if (massive.Length==8)
+{
+    Console.Write("[");
+    for (int i=0;i<massive.Length;i++)
+{
+    if (Convert.ToInt32(massive[i])>=0 && i!=7)
+    {
+        Console.Write($"{massive[i]}, ");
+    }
+     if (Convert.ToInt32(i)==7)
+    {
+        Console.Write($"{massive[i]}");
+
+    }
+
+}
+Console.Write("]");
+}
+
+}
+
+Console.Clear();
+Console.Write("Введите 8 элементов через пробел => ");
+string[] mas = Console.ReadLine().Split(new Char[] { ' ' });
+Massive(mas);
